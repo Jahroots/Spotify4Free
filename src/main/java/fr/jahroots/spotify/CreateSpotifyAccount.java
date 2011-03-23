66850c0ed1bc7d6e62ed227e5ad78d5761f429ab
Jahroots <Jahroots972@gmail.com>
2011-03-23 14:06:51 +0100

    Extract constant variables

--- src/main/java/fr/jahroots/spotify/CreateSpotifyAccount.java
+++ src/main/java/fr/jahroots/spotify/CreateSpotifyAccount.java
@@ -20,8 +20,18 @@ public class CreateSpotifyAccount {
     private static final String PASSWORD_NEW_ID = "password_new";
     private static final String PASSWORD_CHECK_ID = "password_check";
     private static final CharSequence PASSWORD = "azerty";
+    private static final String POSTAL_CODE_ID = "postal_code";
+    private static final String GENDER_ID = "gender";
+    private static final String BIRTHDAY_ID = "birth_day";
+    private static final String BIRTHMONTH_ID = "birth_month";
+    private static final String BIRTHYEAR_ID = "birth_year";
+    private static final String IAGREE_ID = "iagree";
+    private static final String EMAIL_ID = "email";
+
+
 //    private static final String MY_PROXY = "192.168.10.2:8080";
-    
+
+
     private WebDriver driver;
 
     /**
@@ -42,7 +52,7 @@ public class CreateSpotifyAccount {
     	final FirefoxProfile profile = FFProfile();
         driver = new FirefoxDriver(profile);
     }
-    
+
     /**
      * Create an account
      * @param username
@@ -64,31 +74,31 @@ public class CreateSpotifyAccount {
          final WebElement nextStepElement = driver.findElement(By.xpath("//button[@type='submit']"));
          nextStepElement.click();
 
-         final WebElement emailElement = driver.findElement(By.id("email"));
+         final WebElement emailElement = driver.findElement(By.id(EMAIL_ID));
          emailElement.sendKeys(email);
 
-         final WebElement codePostalElement = driver.findElement(By.id("postal_code"));
+         final WebElement codePostalElement = driver.findElement(By.id(POSTAL_CODE_ID));
          codePostalElement.sendKeys(codePostal);
 
-         final WebElement genderElement = driver.findElement(By.id("gender"));
+         final WebElement genderElement = driver.findElement(By.id(GENDER_ID));
          final List<WebElement> genderElements = genderElement.findElements(By.tagName("option"));
          genderElements.get(2).setSelected();
 
-         final WebElement birthDayElement = driver.findElement(By.id("birth_day"));
+         final WebElement birthDayElement = driver.findElement(By.id(BIRTHDAY_ID));
          final List<WebElement> bDayElements = birthDayElement.findElements(By.tagName("option"));
          bDayElements.get(1).setSelected();
 
-         final WebElement birthMonthElement = driver.findElement(By.id("birth_month"));
+         final WebElement birthMonthElement = driver.findElement(By.id(BIRTHMONTH_ID));
          final List<WebElement> bDayMonthElements = birthMonthElement.findElements(By.tagName("option"));
          bDayMonthElements.get(3).setSelected();
 
-         final WebElement birthYearElement = driver.findElement(By.id("birth_year"));
+         final WebElement birthYearElement = driver.findElement(By.id(BIRTHYEAR_ID));
          final List<WebElement> bDayYearhElements = birthYearElement.findElements(By.tagName("option"));
          final int currentYear = Calendar.getInstance().get(Calendar.YEAR);
          final int myAge = currentYear - 1982;
          bDayYearhElements.get(myAge).setSelected();
 
-         final WebElement iagreeElement = driver.findElement(By.id("iagree"));
+         final WebElement iagreeElement = driver.findElement(By.id(IAGREE_ID));
          iagreeElement.click();
 
          final WebElement nextStep2Element = driver.findElement(By.xpath("//div[@id='new_user_step_2_button']/button"));
@@ -96,7 +106,7 @@ public class CreateSpotifyAccount {
 
          driver.findElement(By.id("continue")).click();
     }
-    
+
     /**
      * Create x accounts
      * @param username
@@ -109,7 +119,7 @@ public class CreateSpotifyAccount {
     		createAccount(username + "_" + i, email, codePostal);
     	}
     }
-    
+
     public static void main(String[] args) {
     	new CreateSpotifyAccount().createAccount("BetaTest_1", "BetaTest_1@beta.fr", "75009");
     }
